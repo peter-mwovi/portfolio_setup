923a4321d4346df2a05e2e01ed6d031844008a3b
kambereBr <brunokambere@gmail.com>
2023-06-20 21:41:14 +0200

    Add event listeners to menu links

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const targetDiv = document.getElementById("web-nav");
 const btn = document.getElementById("toggle");
 const cancel = document.getElementById("cross");
+const linkMenu = document.getElementsByClassName('menu-link');
+targetDiv.style.display = 'none';
 
 btn.onclick = function () {
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
   } else {
-    targetDiv.style.display = "block";
+    targetDiv.style.display = "inline";
   }
 };
 
@@ -16,4 +18,11 @@ cancel.onclick = function () {
   } else {
     targetDiv.style.display = "block";
   }
+}
+
+for (let link of linkMenu) {
+  link.addEventListener('click', function() {
+    targetDiv.style.display = "none";
+    window.location.href = link.href;
+  });
 }
