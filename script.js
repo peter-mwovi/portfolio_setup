cdc247de57b04590a69ea078d932c02e78354a37
mwovi <petermwovi73@gmail.com>
2023-06-23 22:41:04 +0300

    Edit script.js file to implement linters suggestions.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const targetDiv = document.getElementById("web-nav");
-const btn = document.getElementById("toggle");
-const cancel = document.getElementById("cross");
-const linkMenu = document.getElementsByClassName("menu-link");
-const form = document.getElementById("contact");
-const email = document.getElementById("email");
-const errMsgEmail = document.querySelector("small");
+const targetDiv = document.getElementById('web-nav');
+const btn = document.getElementById('toggle');
+const cancel = document.getElementById('cross');
+const linkMenu = document.getElementsByClassName('menu-link');
+const form = document.getElementById('contact');
+const email = document.getElementById('email');
+const errMsgEmail = document.querySelector('small');
 
 let emailAddress = email.value;
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   const regex = /[A-Z]/;
   if (regex.test(emailAddress)) {
-    errMsgEmail.innerText = "The Email should only have lower case letters";
+    errMsgEmail.innerText = 'The Email should only have lower case letters';
     e.preventDefault();
   }
 });
@@ -19,7 +19,7 @@ form.addEventListener("submit", (e) => {
 email.addEventListener("input", (e) => {
   emailAddress = e.target.value;
   if (emailAddress === "") {
-    errMsgEmail.innerText = "Email address is required";
+    errMsgEmail.innerText = 'Email address is required';
   } else {
     errMsgEmail.innerText = "";
   }
@@ -28,8 +28,8 @@ email.addEventListener("input", (e) => {
 console.log(emailAddress);
 
 btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
+  if (targetDiv.style.display !== 'none') {
+    targetDiv.style.display = 'none';
     document.body.style.overflowY = "hidden";
   } else {
     targetDiv.style.display = "inline";
@@ -38,19 +38,19 @@ btn.onclick = function () {
 };
 
 cancel.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
-    document.body.style.overflowY = "scroll";
+  if (targetDiv.style.display !== 'none') {
+    targetDiv.style.display = 'none';
+    document.body.style.overflowY = 'scroll';
   } else {
-    targetDiv.style.display = "block";
-    document.body.style.overflowY = "scroll";
+    targetDiv.style.display = 'block';
+    document.body.style.overflowY = 'scroll';
   }
 };
 
 for (let link of linkMenu) {
-  link.addEventListener("click", function () {
-    targetDiv.style.display = "none";
+  link.addEventListener('click', function () {
+    targetDiv.style.display = 'none';
     window.location.href = link.href;
-    document.body.style.overflowY = "scroll";
+    document.body.style.overflowY = 'scroll';
   });
 }
